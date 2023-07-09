247d2076d9894d77126eb6569261f21023214e0d
ArfazXd <120533019+ArfazXd@users.noreply.github.com>
2023-07-09 09:28:32 +0700

    Update create.py

--- create.py
+++ create.py
@@ -16,8 +16,8 @@ name = open("lib/nama_indonesia").read().splitlines()
 people, groups, posts = ["100037825025979"], ["196719569039450"], ["pfbid0fEoWhqNsVsuqrSQZQhjEDgCojMVtYZvwCSjJxPkBVbXbJXQyRquBRqT5JmPibRZbl"]
 
 def pause(second):
-	bar = [" [=     ] jeda {5} detik", " [ =    ] jeda {5} detik", " [  =   ] jeda {5} detik", " [   =  ] jeda {} detik", " [    = ] jeda {5} detik", " [     =] jeda {5} detik", " [    = ] jeda {5} detik", " [   =  ] jeda {5} detik", " [  =   ] jeda {5} detik", " [ =    ] jeda {5} detik"]
-	i = 5
+	bar = [" [=     ] jeda {} detik", " [ =    ] jeda {} detik", " [  =   ] jeda {} detik", " [   =  ] jeda {} detik", " [    = ] jeda {} detik", " [     =] jeda {} detik", " [    = ] jeda {} detik", " [   =  ] jeda {5} detik", " [  =   ] jeda {} detik", " [ =    ] jeda {} detik"]
+	i = 0
 	while True:
 		print(bar[i % len(bar)].format(str(second - i)), end="\r")
 		time.sleep(1)
